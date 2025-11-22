fe2fb2c216dca70c9a2b5841a4cc5928eeede8f3
Pavel Satunovskiy <gardsilver@list.ru>
2025-11-22 05:02:30 +0300

    add ws connect status for chat

--- front/static/app.js
+++ front/static/app.js
@@ -4,6 +4,7 @@ const msgBox = document.getElementById('exampleFormControlTextarea1');
 const msgCont = document.getElementById('data-container');
 const email = document.getElementById('to-email');
 const fromEmail = document.getElementById('from-email');
+const connectStatus = document.getElementById('from-email').parentElement.firstElementChild.firstElementChild;
 const messages = [];
 
 fromEmail.addEventListener('keydown', (e) => {
@@ -15,6 +16,36 @@ fromEmail.addEventListener('keydown', (e) => {
         "send-from": e.target.value,
       }
     });
+
+    socket.io.on("error", () => {
+      connectStatus.classList.remove("btn-outline-default", "btn-outline-warning", "btn-outline-success", "btn-outline-danger")
+      connectStatus.classList.add("btn-outline-danger")
+    })
+
+    socket.io.on("reconnect", () => {
+      connectStatus.classList.remove("btn-outline-default", "btn-outline-warning", "btn-outline-success", "btn-outline-danger")
+      connectStatus.classList.add("btn-outline-warning")
+    })
+
+    socket.io.on("reconnect_error", () => {
+      connectStatus.classList.remove("btn-outline-default", "btn-outline-warning", "btn-outline-success", "btn-outline-danger")
+      connectStatus.classList.add("btn-outline-danger")
+    })
+
+    socket.io.on("reconnect_failed", () => {
+      connectStatus.classList.remove("btn-outline-default", "btn-outline-warning", "btn-outline-success", "btn-outline-danger")
+      connectStatus.classList.add("btn-outline-danger")
+    })
+
+    socket.on("connect", () => {
+      connectStatus.classList.remove("btn-outline-default", "btn-outline-warning", "btn-outline-success", "btn-outline-danger")
+      connectStatus.classList.add("btn-outline-success")
+    })
+
+     socket.on("disconnect", () => {
+      connectStatus.classList.remove("btn-outline-default", "btn-outline-warning", "btn-outline-success", "btn-outline-danger")
+      connectStatus.classList.add("btn-outline-danger")
+    })
 
     socket.on('answerMessage', (message) => {
       messages.push(message);
@@ -44,22 +75,19 @@ function loadDate(data) {
     switch (message.status) {
       case "send":
         messages += 
-        ` <li class="bg-success p-2 rounded mb-2 text-light">
-          <span class="fw-bolder">${message.email}</span>
-          ${message.text}
+        `<li class="bg-success p-2 rounded mb-2 text-light">
+          <span class="fw-bolder">${message.from}</span> to <span class="fw-bolder">${message.to}</span>: ${message.text}
         </li>`
         break;
       case "error":
         messages += 
         ` <li class="bg-danger p-2 rounded mb-2 text-light">
-          <span class="fw-bolder">${message.email}</span>
-          ${message.text}
+          <span class="fw-bolder">${message.from}</span> to <span class="fw-bolder">${message.to}</span>: ${message.text}
         </li>`;
         break;
       default:
         messages += ` <li class="bg-primary  p-2 rounded mb-2 text-light">
-          <span class="fw-bolder">${message.email}</span>
-          ${message.text}
+          <span class="fw-bolder">${message.from}</span> to <span class="fw-bolder">${message.to}</span>: ${message.text}
         </li>`;
         break;
     }

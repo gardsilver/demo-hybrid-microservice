84a4f39e36648ffb6009af66bab870b78e435773
gardsilver <32588659+gardsilver@users.noreply.github.com>
2025-11-26 08:32:39 +0300

    rename end-points for WS (#37)

--- src/core/app/controller/chat.controller.ts
+++ src/core/app/controller/chat.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Render, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { SkipInterceptors } from 'src/modules/common';
 
 @SkipInterceptors({ HttpAuthGuard: true })
+@ApiTags('chat')
 @Controller()
-export class AppController {
+export class ChatController {
   @Get('/chat')
   @Render('index')
   home() {}
